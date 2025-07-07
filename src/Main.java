f9dc25a7fbeb8875cccd9f9c909f071cb2aafca7
RyoHaSeu99 <cmrhanc123@naver.com>
2025-07-07 15:23:24 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Thread worker = new Thread(() -> {
+            try {
+                System.out.println("Worker: 작업 시작");
+
+                Thread.sleep(10_000);
+
+                System.out.println("Worker: 작업 완료");
+            } catch(InterruptedException e) {
+                System.out.println("Worker: 중단 신호 수신, 정리 작업 후 종료");
+            }
+        });
+    }
+}
